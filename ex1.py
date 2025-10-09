b9134019ecc1c42e6634bcfbfe090ae77ec66b4c
Pedro Henrique de Souza Silva <iampedro26@gmail.com>
2025-10-09 20:03:25 -0300

    mais exercicios

--- ex1.py
+++ ex1.py
@@ -1,3 +1,40 @@
+#explicações importantes:
+#tipos de dados: int, float, str e bool
+#int = numeros inteiros
+#float = numeros com ponto flutuante
+#str = strings, textos
+#bool = booleano, true ou false
+
+#################################################################################################################
+#EXERCICIOS DE PYTHON
+#################################################################################################################
+
+#calculadora basica
+
+num1 = 10
+num2 = 20
+
+soma = num1 + num2
+subtracao = num1 - num2
+multiplicacao = num1 * num2
+divisao = num1 / num2
+resto_da_divisao = num1 % num2
+
+print(f"Soma = ", soma)
+print(f"Subtração = ", subtracao)
+print(f"Multiplicação = ", multiplicacao)
+print(f"Divisão = ", divisao)
+print(f"Resto da divisão = ", resto_da_divisao)
+
+#exercicio mensagem personaliazada
+
+nome = "Pedro"
+idade = 20
+altura = 1.75
+peso = 75
+
+print(f"Olá, meu nome é {nome}, tenho {idade} anos, minha altura é {altura}m e meu peso é {peso}kg")
+
 #exercicio calcular maioridade 
 
 idade = 20
@@ -13,8 +50,8 @@ else:
 
 #exercicio calcular imc
 
-peso = 5000
-altura = 4.54
+peso = 76
+altura = 1.75
 
 imc = peso / (altura ** 2)
 
@@ -25,16 +62,19 @@ elif imc >= 18.5 and imc < 24.9:
     print("Peso normal")
 
 elif imc <= 25 and imc < 29.9:
-    print("Gordona Miseravel")
+    print("Sobrepeso")
 
 elif imc <= 30 and imc < 34.9:
-    print("voce é GORDA porra")
+    print("Obesidade grau 1")
 
 elif imc <= 35 and imc < 39.9:
-    print("BALEIA JUBARTE")
+    print("Obesidade grau 2")
 
 elif imc >= 40:
-    print("MR PUSSY")
+    print("Obesidade grau 3")
+
+elif imc > 60:
+    print("Obesidade mórbida")
 
 else:
     print("Morreu")
@@ -42,3 +82,21 @@ else:
 print(f"Seu imc: {imc:2f}")
 
 #exercicio calcular par ou impar
+
+num = 10
+
+if num % 2 == 0:
+    print("Numero par")
+
+else:
+    print("Numero impar")
+
+#exercicio conversor de metros
+
+metros = 100
+
+centimetros = metros * 100
+milimetros = metros * 1000
+km = metros / 1000
+
+print(f"{metros} metros equivalem a {centimetros}, {milimetros} milimetros e {km} km")
