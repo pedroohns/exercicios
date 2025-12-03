23360325feacdfa70f0c054a9282422498f529d4
Pedro Henrique de Souza Silva <iampedro26@gmail.com>
2025-12-03 18:20:29 -0300

    finalizado o conversor de temperatura

--- temperatura.js
+++ temperatura.js
@@ -2,3 +2,16 @@ const celsius = document.getElementById('celsius');
 const btn = document.getElementById('converter');
 const fahrenheit = document.getElementById('fahrenheit');
 
+btn.addEventListener('click', () => {
+    const raw = celsius.value;
+    console.log('valor digitado', raw);
+
+const temperatura = Number(raw);
+console.log(temperatura);
+
+const resultado = temperatura * 1.8 + 32;
+console.log(resultado);
+
+fahrenheit.textContent = resultado + " °F";
+
+});
